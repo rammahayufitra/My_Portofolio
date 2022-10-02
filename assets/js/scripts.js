fc7b9d5b0275a6570d16bf189ac82b564353cf87
rammafitra <rammahayufitra@gmail.com>
2022-10-02 23:13:56 +0700

    [okt-2-2022] home-section checkpoint1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,45 @@ menu.onclick = () =>{
     header.classList.toggle('active');
 }
 
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
+
+var typed = new Typed(".auto-typed",{
+    strings: ["ramma fitra"],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    loop:true
+})
+
+var typed = new Typed(".auto-icon",{
+    strings: ["&#128512;", "&#128523;", "&#128525;", "&#128526;", " &#128521;"],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    loop:true
+})
